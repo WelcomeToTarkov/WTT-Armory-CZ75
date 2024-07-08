d7706836103d623752cec5f8c93add432eed43f4
Tron111111122222 <eagentile@sbcglobal.net>
2024-07-08 11:41:43 -0500

    Update to 3.9

--- user/mods/WTT-Armory-CZ75/src/CustomItemService.js
+++ user/mods/WTT-Armory-CZ75/src/CustomItemService.js
@@ -37,7 +37,7 @@ class CustomItemService {
     constructor() {
         this.configs = this.loadCombinedConfig();
     }
-    preAkiLoad(Instance) {
+    preSptLoad(Instance) {
         this.Instance = Instance;
     }
     postDBLoad() {
@@ -58,7 +58,7 @@ class CustomItemService {
             this.processWeaponPresets(itemConfig, itemId);
             this.processBotInventories(itemConfig, finalItemTplToClone, itemId);
             this.processTraders(itemConfig, itemId);
-            //this.modifyQuests(tables, jsonUtil);
+            this.modifyQuests(this.Instance.database, this.Instance.jsonUtil);
             numItemsAdded++;
         }
         if (numItemsAdded > 0) {
@@ -357,6 +357,133 @@ class CustomItemService {
                     console.log(` - Preset: ${JSON.stringify(preset)}`);
                 }
             });
+        }
+    }
+    /**
+ * Modify the quests in the given tables using the provided JSON utility.
+ *
+ * @param {IDatabaseTables} tables - the tables containing the quests
+ * @param {JsonUtil} jsonUtil - the JSON utility for cloning objects
+ * @return {void}
+ */
+    modifyQuests(tables, jsonUtil) {
+        // Define new pistols
+        const newPistols = [
+            "6661012d16fbd2fb75408f87"
+        ];
+        // Get the specific quest
+        const stirrup = tables.templates.quests["596b455186f77457cb50eccb"];
+        if (stirrup) {
+            // Extract existing pistols
+            const existingStirrupPistols = stirrup.conditions.AvailableForFinish[0].counter.conditions[0].weapon;
+            try {
+                // Clone the existing pistols array
+                const updatedPistols = jsonUtil.clone(existingStirrupPistols);
+                let modified = false;
+                // Add new pistols if they do not already exist
+                for (const pistol of newPistols) {
+                    if (!updatedPistols.includes(pistol)) {
+                        updatedPistols.push(pistol);
+                        modified = true;
+                        if (this.Instance.debug) {
+                            console.log("Added new pistol:", pistol);
+                        }
+                    }
+                    else {
+                        if (this.Instance.debug) {
+                            console.log("Pistol already exists:", pistol);
+                        }
+                    }
+                }
+                // Only update the quest if modifications were made
+                if (modified) {
+                    stirrup.conditions.AvailableForFinish[0].counter.conditions[0].weapon = updatedPistols;
+                    if (this.Instance.debug) {
+                        console.log("Modified quest:", stirrup.conditions.AvailableForFinish[0].counter.conditions[0].weapon);
+                    }
+                }
+            }
+            catch (error) {
+                console.error("Error modifying quests:", error);
+            }
+        }
+        // Get the specific quest
+        const mallCop = tables.templates.quests["64e7b99017ab941a6f7bf9d7"];
+        if (mallCop) {
+            // Extract existing pistols
+            const existingmallCopPistols = mallCop.conditions.AvailableForFinish[0].counter.conditions[0].weapon;
+            try {
+                // Clone the existing pistols array
+                const updatedPistols = jsonUtil.clone(existingmallCopPistols);
+                let modified = false;
+                // Add new pistols if they do not already exist
+                for (const pistol of newPistols) {
+                    if (!updatedPistols.includes(pistol)) {
+                        updatedPistols.push(pistol);
+                        modified = true;
+                        if (this.Instance.debug) {
+                            console.log("Added new pistol:", pistol);
+                        }
+                    }
+                    else {
+                        if (this.Instance.debug) {
+                            console.log("Pistol already exists:", pistol);
+                        }
+                    }
+                }
+                // Only update the quest if modifications were made
+                if (modified) {
+                    mallCop.conditions.AvailableForFinish[0].counter.conditions[0].weapon = updatedPistols;
+                    if (this.Instance.debug) {
+                        console.log("Modified quest:", mallCop.conditions.AvailableForFinish[0].counter.conditions[0].weapon);
+                    }
+                }
+            }
+            catch (error) {
+                console.error("Error modifying quests:", error);
+            }
+        }
+        // Define new suppressors
+        const newSuppressors = [
+            "666102ef2d367ec85ddd2030"
+        ];
+        // Get the specific quest
+        const punisher2 = tables.templates.quests["59c50c8886f7745fed3193bf"];
+        if (punisher2) {
+            // Extract existing suppressors
+            const existingPunisher2Suppressors = punisher2.conditions.AvailableForFinish[0].counter.conditions[0].weaponModsInclusive;
+            try {
+                // Clone the existing suppressors array
+                const updatedSuppressors = jsonUtil.clone(existingPunisher2Suppressors);
+                let modified = false;
+                // Flatten the nested arrays for easier checking
+                const flattenedSuppressors = updatedSuppressors.flat();
+                // Add new suppressors if they do not already exist
+                for (const suppressor of newSuppressors) {
+                    if (!flattenedSuppressors.includes(suppressor)) {
+                        updatedSuppressors.push([suppressor]);
+                        modified = true;
+                        if (this.Instance.debug) {
+                            console.log("Added new suppressor:", suppressor);
+                        }
+                    }
+                    else {
+                        if (this.Instance.debug) {
+                            console.log("Suppressor already exists:", suppressor);
+                        }
+                    }
+                }
+                // Only update the quest if modifications were made
+                if (modified) {
+                    punisher2.conditions.AvailableForFinish[0].counter.conditions[0].weaponModsInclusive = updatedSuppressors;
+                    if (this.Instance.debug) {
+                        console.log("Modified quest:", punisher2.conditions.AvailableForFinish[0].counter.conditions[0].weaponModsInclusive);
+                    }
+                }
+            }
+            catch (error) {
+                console.error("Error modifying quests:", error);
+            }
         }
     }
     /**
